4bdb048e6b6d3b6786e6a42be1d24309e8d02b3e
Hemang Subramanian <hemangcs@gmail.com>
2013-01-11 15:08:45 -0500

    first windows commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaislDemo::Application.config.secret_token = '1603e1a180b9ff3e24f7c219d42fe6662d7cc20b1d1d092e08c15277a80979f632e0783f966a4aac8262b0278181ccd333c69ca883bd4b258c5d05b1805c2026'
